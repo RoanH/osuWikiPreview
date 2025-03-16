13909d3a13decc1fd34cbd62cf0624bde7bc2092
Roan Hofland <roan@roanh.dev>
2025-03-16 22:08:13 +0100

    Add javadoc to the web state class

--- osuWikiPreview/src/dev/roanh/wiki/WebState.java
+++ osuWikiPreview/src/dev/roanh/wiki/WebState.java
@@ -51,10 +51,21 @@ public class WebState{
 	 * Whether ppy/master was merged into the ref beforehand.
 	 */
 	private boolean master;
+	/**
+	 * The pull request associated with the current ref if any.
+	 */
 	private PullRequest pr;
+	/**
+	 * The instant the web instance becomes available for automatic claiming.
+	 */
 	private Instant available;
 	
-	public WebState(ResultSet rs) throws SQLException{
+	/**
+	 * Constructs a new web state from the given result set. 
+	 * @param rs The result set to read from.
+	 * @throws SQLException When an SQL exception occurs.
+	 */
+	protected WebState(ResultSet rs) throws SQLException{
 		long prId = rs.getLong("pr_id"); 
 		pr = prId == -1L ? null : new PullRequest(prId, rs.getInt("pr_num"));
 		namespace = rs.getString("namespace");
@@ -64,6 +75,13 @@ public class WebState{
 		available = Instant.ofEpochSecond(rs.getLong("available"));
 	}
 	
+	/**
+	 * Constructs a new web state.
+	 * @param namespace The namespace for the new state.
+	 * @param ref The ref for the new state (commit/branch/tag).
+	 * @param redate True to redate newsposts.
+	 * @param master True to merge ppy/master into the preview.
+	 */
 	private WebState(String namespace, String ref, boolean redate, boolean master){
 		this.namespace = namespace;
 		this.ref = ref;
@@ -81,46 +99,95 @@ public class WebState{
 		return "https://github.com/" + namespace + "/osu-wiki/tree/" + ref;
 	}
 	
+	/**
+	 * Gets the namespace for the current web state.
+	 * @return The namespace (users / organisation).
+	 */
 	public String getNamespace(){
 		return namespace;
 	}
 	
+	/**
+	 * Gets the combination of the namespace and ref.
+	 * @return The namespace and ref for this state.
+	 * @see #getNamespace()
+	 * @see #getRef()
+	 */
 	public String getNamespaceWithRef(){
 		return namespace + "/" + ref;
 	}
 	
+	/**
+	 * Checks if news redating is enabled for the preview.
+	 * @return True if news is redated.
+	 */
 	public boolean hasRedate(){
 		return redate;
 	}
 	
+	/**
+	 * Checks if ppy/master is merged into the preview.
+	 * @return True if master is merged into the preview.
+	 */
 	public boolean hasMaster(){
 		return master;
 	}
 	
+	/**
+	 * Gets the ref shown on the preview.
+	 * @return The ref being previewed.
+	 */
 	public String getRef(){
 		return ref;
 	}
 	
+	/**
+	 * Checks if the current ref as an associated pull request
+	 * on the official osu! wiki repository.
+	 * @return True if a pull request exists.
+	 */
 	public boolean hasPullRequest(){
 		return pr != null;
 	}
 	
+	/**
+	 * Gets the pull request for the current ref if any.
+	 * @return True pull request for the current ref.
+	 * @see #hasPullRequest()
+	 */
 	public Optional<PullRequest> getPullRequest(){
 		return Optional.ofNullable(pr);
 	}
 	
+	/**
+	 * Sets the pull request for the current ref.
+	 * @param pr The pull request for the current ref.
+	 */
 	public void setPullRequest(PullRequest pr){
-		
+		this.pr = pr;
 	}
 	
+	/**
+	 * Sets the pull request for the current ref.
+	 * @param pr The pull request for the current ref.
+	 */
 	public void setPullRequest(PullRequestInfo pr){
 		setPullRequest(new PullRequest(pr.id(), pr.number()));
 	}
 	
+	/**
+	 * Checks when this preview instance becomes available for automatic claiming.
+	 * @return The instant this preview instance becomes available.
+	 */
 	public Instant getAvailableAt(){
 		return available;
 	}
 	
+	/**
+	 * Locks this instance for at least the given duration.
+	 * @param duration The duration to claim this instance for.
+	 * @see #getAvailableAt()
+	 */
 	public void refreshClaim(Duration duration){
 		available = Instant.now().plus(duration);
 	}
@@ -151,10 +218,23 @@ public class WebState{
 		return namespace.equals("RoanH") && ref.startsWith("wikisync-");
 	}
 	
+	/**
+	 * Constructs a new web state for showing manually uploaded newspost preview.
+	 * @param web The instance the preview will be on.
+	 * @return The constructed web state.
+	 */
 	public static final WebState forNewspostPreview(OsuWeb web){
 		return new WebState("RoanH", web.getWikiSyncBranch(), true, false);
 	}
 	
+	/**
+	 * Constructs a new web state for showing the given ref.
+	 * @param namespace The namespace for the ref.
+	 * @param ref The ref to preview.
+	 * @param redate True to redate any news.
+	 * @param master True to merge ppy/master into the preview.
+	 * @return The constructed web state.
+	 */
 	public static final WebState forRef(String namespace, String ref, boolean redate, boolean master){
 		return new WebState(namespace, ref, redate, master);
 	}
